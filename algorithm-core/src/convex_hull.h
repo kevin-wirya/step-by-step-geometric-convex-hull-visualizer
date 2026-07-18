a9f0d639febedf5a5fb393c39ff880356c6e5194
Kevin <kevin.wirya.valerian@gmail.com>
2026-07-18 09:46:06 +0700

    feat: define point and line structure

--- algorithm-core/src/convex_hull.h
+++ algorithm-core/src/convex_hull.h
@@ -1,1 +1,17 @@
-
+#ifndef CONVEX_HULL_H
+#define CONVEX_HULL_H
+#include"point.h"
+#include<string>
+#include<vector>
+using namespace std;
+struct Line {
+    Point p1, p2;
+};
+struct AlgorithmicStep {
+    string description;
+    vector<Point> hull;
+    vector<Point> active;
+    vector<Point> discarded;
+    vector<Line> activeLines;
+};
+#endif
--- algorithm-core/src/point.h
+++ algorithm-core/src/point.h
@@ -1,1 +1,9 @@
-
+#ifndef POINT_H
+#define POINT_H
+#include <string>
+using namespace std;
+struct Point {
+    double x, y;
+    string id;
+};
+#endif
